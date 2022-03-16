ace44814435eb01277ceeab7d2018f0c17321a9c
Alexandre Camilo <alex.camilo6@hotmail.ch>
2022-03-16 13:53:12 +0100

    1st part finish

--- Gemfile
+++ Gemfile
@@ -52,3 +52,7 @@ gem 'font-awesome-sass', '~> 5.6.1'
 gem 'simple_form', github: 'heartcombo/simple_form'
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
+
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
+
+gem 'simple_form', github: 'heartcombo/simple_form'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+# 10.times do
+#   Article.create(title: Faker::Book.title,
+#     content: Faker::Lorem.characters(number: 10))
+# end
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "italian", phone_number:"01 02 03 04" }
+pizza_east =  { name: "Pizza East", address: "56E Shoreditch High St, London E1 6PQ", category: "chinese", phone_number:"01 02 03 04" }
+pizza_north =  { name: "Pizza North", address: "12N Shoreditch High St, London E1 6PQ", category: "french", phone_number:"01 02 03 04" }
+pizza_south =  { name: "Pizza South", address: "100S Shoreditch High St, London E1 6PQ", category: "japanese", phone_number:"01 02 03 04" }
+pizza_west =  { name: "Pizza West", address: "98W Shoreditch High St, London E1 6PQ", category: "belgian", phone_number:"01 02 03 04" }
+
+[ dishoom, pizza_east, pizza_north, pizza_south, pizza_west ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
